a396f968dbe9a7651e21f035f77c76b931f00a45
dzhibas <nikolajus@gmail.com>
2023-12-04 10:11:39 +0100

    tmux navigator, dracula

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -73,6 +73,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  'christoomey/vim-tmux-navigator',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
